73ff0b7c2f6b03a132bcb8edd80728c05448327f
tigerFish29 <paulwchabz28@gmail.com>
2022-08-01 16:26:00 +0100

    made changes to user.java

--- src/main/java/com/example/demo/domains/User.java
+++ src/main/java/com/example/demo/domains/User.java
@@ -1,5 +1,97 @@
 package com.example.demo.domains;
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+
+@Entity
 public class User {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private LocalDate dob;
+
+    // default constructor 
+    public User() {
+        super();
+    }
+
+    // main constructor 
+    public User(String username, String password, String firstName, String lastName, LocalDate dob) {
+        super();
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName; 
+        this.lastName = lastName;
+        this.dob = dob;
+    }
+
+    // setters and getters 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return username;
+    }
+
+    public void setUserName(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+
 }
