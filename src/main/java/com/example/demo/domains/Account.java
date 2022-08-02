5843b42efd176daaa630f90dc2b1957872007861
tigerFish29 <paulwchabz28@gmail.com>
2022-08-02 10:10:32 +0100

    added an account entity

---
+++ src/main/java/com/example/demo/domains/Account.java
@@ -0,0 +1,104 @@
+package com.example.demo.domains;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String accountNumber;
+
+    @Column
+    private String name; 
+
+    // default constructor 
+    public Account() {
+        super();
+    }
+
+    // main constructor 
+    public Account(String name, Long id, String accountNumber) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.accountNumber = accountNumber;
+    }
+
+    // getters and setters 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Account [accountNumber=" + accountNumber + ", id=" + id + ", name=" + name + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((accountNumber == null) ? 0 : accountNumber.hashCode());
+        result = prime * result + (int) (id ^ (id >>> 32));
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Account other = (Account) obj;
+        if (accountNumber == null) {
+            if (other.accountNumber != null)
+                return false;
+        } else if (!accountNumber.equals(other.accountNumber))
+            return false;
+        if (id != other.id)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
+    
+
+    
+    
+}
